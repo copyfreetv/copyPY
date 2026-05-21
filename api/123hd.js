8a5f097081013abc00c3355b6c2963428d7ff1a6
copyfreetv <copytv.iptv@gmail.com>
2026-05-21 07:43:50 +0700

    Update 123hd.js

--- api/123hd.js
+++ api/123hd.js
@@ -1,65 +1,34 @@
 export default async function handler(req, res) {
 
-  const target =
-  req.query.url;
+  const target = req.query.url;
 
-  if(!target){
+  if (!target) {
 
-    return res.status(400).json({
-      error:"Missing url"
-    });
+    return res.status(400).send("missing url");
 
   }
 
-  try{
+  try {
 
-    const response =
-    await fetch(target,{
+    const response = await fetch(target, {
 
-      headers:{
+      headers: {
 
         "User-Agent":
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140 Safari/537.36",
+        "Mozilla/5.0",
 
         "Referer":
         "https://www.123-hds.com/",
 
         "Origin":
-        "https://www.123-hds.com",
-
-        "Accept":
-        "*/*",
-
-        "Accept-Language":
-        "en-US,en;q=0.9",
-
-        "Cache-Control":
-        "no-cache",
-
-        "Pragma":
-        "no-cache",
-
-        "Sec-Fetch-Dest":
-        "empty",
-
-        "Sec-Fetch-Mode":
-        "cors",
-
-        "Sec-Fetch-Site":
-        "cross-site"
+        "https://www.123-hds.com"
 
       }
 
     });
 
-
-
-
     const text =
     await response.text();
-
-
-
 
     res.setHeader(
       "Access-Control-Allow-Origin",
@@ -71,17 +40,11 @@ export default async function handler(req, res) {
       "text/html; charset=utf-8"
     );
 
-
-
     res.status(200).send(text);
 
-  }catch(err){
+  } catch (e) {
 
-    res.status(500).json({
-
-      error:err.message
-
-    });
+    res.status(500).send(e.toString());
 
   }
 
